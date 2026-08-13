e815e6d1511be326cf2020787c443edd57303dd3
In Frame Medias <ccadbury470@gmail.com>
2026-08-13 23:14:50 +0530

    Fix footer import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import Clients from "./components/Clients";
 import { useState } from "react";
 import ProjectForm from "./components/ProjectForm";
